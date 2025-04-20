4f57162f45f8bdbabe2f690d970439caead16b92
nanika4 <ok.atk04@gmail.com>
2025-04-20 15:19:06 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function pressButton() {
+  const messageBox = document.getElementById("message");
+  const form = document.getElementById("winner-form");
+  const share = document.getElementById("share");
+  const chance = 124500000;
+  const isWinner = Math.floor(Math.random() * chance) === 0;
+
+  const button = document.getElementById("button");
+  button.style.pointerEvents = "none";
+
+  if (isWinner) {
+    messageBox.textContent = "アナタノユメカナウ";
+    messageBox.className = "show winner";
+    form.classList.remove("hidden");
+  } else {
+    messageBox.textContent = "ハズレ、アシタユメカナウ";
+    messageBox.className = "show";
+    share.classList.remove("hidden");
+
+    // SNSテンプレートのURL生成
+    const text = encodeURIComponent("このボタン、押したら「ユメボタン」だった。
+#ユメボタン #夢か叶うボタン");
+    const url = encodeURIComponent(window.location.href);
+    document.getElementById("share-x").href = `https://twitter.com/intent/tweet?text=${text}&url=${url}`;
+  }
+}
